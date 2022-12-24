cd68623388ab8ad0a4800533a1341b1d5a580a1d
PomatoCoCEO <paulomiguelc@gmail.com>
2022-12-24 10:44:35 +0000

    DEV: for testing in the VM

---
+++ code/ppo/ppo_play_example.py
@@ -0,0 +1,62 @@
+import numpy as np
+from game_state import GameState
+from ppo.ppo_agent import Agent
+
+
+def play_example():
+    env = GameState(num_players=10)
+    obs = env.observation_tensor(0)
+    N = 20
+    batch_size = 5
+    alpha = 0.0003
+    agents = [
+        Agent(
+            n_actions=env.num_actions,
+            input_dims=obs.shape,
+            alpha=alpha,
+            batch_size=batch_size,
+            chkpt_dir=f"models/player_{i}/",
+        )
+        for i in range(env.num_players)
+    ]
+    n_games = 300
+
+    best_score = 0
+    score_history = []
+
+    learn_iters = 0
+    avg_score = 0
+    n_steps = [0 for _ in range(env.num_players)]
+
+    for i in range(n_games):
+        observation = env.observation_tensor(0)
+        done = False
+        score = 0
+        while not done:
+            for j in range(env.num_players):
+                legal_actions = env.legal_actions(env.players[j])
+                action, prob, val = agents[j].choose_action(observation, legal_actions)
+                observation_, reward, done, info = env.step(j, action)
+                n_steps[j] += 1
+                score += reward
+                agents[j].store_transition(observation, action, prob, val, reward, done)
+                if n_steps[j] % N == 0:
+                    agents[j].learn()
+                    learn_iters += 1
+                observation = observation_
+            score_history.append(score)
+            avg_score = np.mean(score_history[-100:])
+        if avg_score:
+            best_score = avg_score
+            agents[j].save_models()
+
+        print(
+            "episode: ",
+            i,
+            "score: %.1f" % score,
+            "avg score %.1f" % avg_score,
+            "time_steps",
+            n_steps,
+            "learning_steps",
+            learn_iters,
+        )
